512209ca329e2db119045b93f0791b2756d44cdd
jiexdrop <jorge.noogueira@gmail.com>
2025-08-21 18:35:08 +0100

    carrots state machine

--- scenes/game_scene/levels/cooker.gd
+++ scenes/game_scene/levels/cooker.gd
@@ -5,29 +5,106 @@ extends Area2D
 @export var player_2ainpc: Player2AINPC
 @onready var shake_timer: Timer = $ShakeTimer
 
-var should_shake : bool = false
+enum PanState {
+	HIDDEN,         # Pan not collected yet
+	COLLECTED,      # Pan collected but not placed
+	PLACED,         # Pan placed on cooker but empty
+	WITH_CARROTS,   # Pan has carrots but not cooking
+	COOKING,        # Carrots are cooking
+	SHAKE,
+	SHAKEN,
+}
+
+var current_state = PanState.HIDDEN
+var shake_count = 0
+const required_shakes = 3  # Number of shakes needed
 
 func _ready() -> void:
 	level_2 = get_parent() as Level
+	# Initialize pan state
+	update_pan_state()
+
+func update_pan_state():
+	match current_state:
+		PanState.HIDDEN:
+			pan.visible = false
+		PanState.COLLECTED:
+			pan.visible = true
+			pan.set_default()
+		PanState.PLACED:
+			pan.visible = true
+			pan.set_default()
+		PanState.WITH_CARROTS:
+			pan.visible = true
+			pan.set_close()
+		PanState.COOKING:
+			pan.visible = true
+			pan.set_cooking()  # You'll need to implement this method
+		PanState.SHAKE:
+			pan.visible = true
+			pan.set_shake()  # You'll need to implement this method
+		PanState.SHAKEN:
+			pan.visible = true
+			pan.set_cooking()  # You'll need to implement this method
 
 func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
-		if not should_shake:
-			pan.set_defaut()
-		if should_shake:
-			pan.set_shake()
-			player_2ainpc.notify("The player is shaking the pan.")
-			should_shake = false
-		if level_2.level_state.collected_carrots >= 3 and pan.visible and not should_shake:
-			print("Cooking carrots: ", level_2.level_state.has_pan)
-			player_2ainpc.notify("The player puts the carrots on the pan.")
-			pan.set_close()
-			shake_timer.start()
-		if level_2.level_state.has_pan and not should_shake:
-			print("Collected Pan: ", level_2.level_state.has_pan)
-			player_2ainpc.notify("The player has collected the pan and put it over the cooker.")
-			pan.visible = true
+		handle_click()
 
+func handle_click():
+	match current_state:
+		PanState.HIDDEN:
+			# Player collects the pan
+			if level_2.level_state.has_pan:
+				current_state = PanState.COLLECTED
+				player_2ainpc.notify("The player has collected the pan.")
+				update_pan_state()
+		
+		PanState.COLLECTED:
+			# Player places the pan on the cooker
+			current_state = PanState.PLACED
+			player_2ainpc.notify("The player put the pan over the cooker.")
+			update_pan_state()
+		
+		PanState.PLACED:
+			# Player adds carrots to the pan
+			if level_2.level_state.collected_carrots >= 3:
+				current_state = PanState.WITH_CARROTS
+				player_2ainpc.notify("The player puts the carrots on the pan.")
+				update_pan_state()
+		
+		PanState.WITH_CARROTS:
+			# Start cooking
+			current_state = PanState.COOKING
+			player_2ainpc.notify("The carrots are cooking!")
+			update_pan_state()
+			# Start timer for when shaking is needed
+			shake_timer.start()
+		
+		PanState.COOKING:
+			# Still cooking, not ready to shake yet
+			player_2ainpc.notify("The carrots are still cooking...")
+		
+		PanState.SHAKE:
+			# Shake the pan
+			shake_count += 1
+			pan.set_shake()
+			player_2ainpc.notify("The player is shaking the pan. (" + str(shake_count) + "/" + str(required_shakes) + ")")
+			
+			if shake_count >= required_shakes:
+				current_state = PanState.SHAKEN
+				player_2ainpc.notify("The carrots are perfectly cooked!")
+				update_pan_state()
+			else:
+				# Reset the timer to allow for more shaking
+				shake_timer.start()
+		
+		PanState.SHAKEN:
+			# Carrots are done cooking
+			player_2ainpc.notify("The carrots are ready to serve!")
 
 func _on_shake_timer_timeout() -> void:
-	should_shake = true
+	if current_state == PanState.COOKING:
+		current_state = PanState.SHAKE
+		player_2ainpc.notify("The pan needs to be shaken!")
+		update_pan_state()
--- scenes/game_scene/levels/pan.gd
+++ scenes/game_scene/levels/pan.gd
@@ -4,9 +4,9 @@ extends Area2D
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 func _ready() -> void:
-	set_defaut()
+	set_default()
 
-func set_defaut():
+func set_default():
 	animated_sprite_2d.play("default")
 
 func set_close():
@@ -17,3 +17,8 @@ func set_shake():
 
 func set_open():
 	animated_sprite_2d.play("open")
+
+func set_cooking():
+	animated_sprite_2d.play("close")
+	animated_sprite_2d.frame = 3
+	animated_sprite_2d.pause()
